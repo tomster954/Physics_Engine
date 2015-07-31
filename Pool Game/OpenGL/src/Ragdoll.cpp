f53ab5d850a12cdbfebde52a871829b5eb65bb64
Tom Solo <tomster954@gmail.com>
2015-07-31 10:30:39 +1000

    ragdoll Drawing with gizmos

--- Pool Game/OpenGL/src/Ragdoll.cpp
+++ Pool Game/OpenGL/src/Ragdoll.cpp
@@ -60,7 +60,8 @@ void Ragdoll::Draw()
 			center.y = link->getWorldBounds().getCenter().y;
 			center.z = link->getWorldBounds().getCenter().z;
 			//glm::mat4 newRot = Utils::TransformToMat4(_actor->getGlobalPose());
-			Gizmos::addSphere(center, radius, 5, 5, glm::vec4(1, 0, 0, 1));
+			//Gizmos::addSphere(center, radius, 5, 5, glm::vec4(1, 0, 0, 1));
+			AddWidget(shapes[nShapes], link);
 		}
 	}
 	delete[] links;
@@ -161,20 +162,72 @@ void Ragdoll::AddWidget(PxShape* shape, PxRigidActor* actor)
 
 void Ragdoll::AddSphere(PxShape* pShape, PxRigidActor* actor)
 {
-	//bool status = pShape->getSphereGeometry(geometry);
-	//if (status)
-	//{
-	//	radius = geometry.radius;
-	//}
+	PxSphereGeometry geometry;
+	glm::vec3 SpherePosition;
+	glm::vec4 SphereColour = glm::vec4(1, 0, 0, 1);
+	float SphereRadius;
+
+	bool status = pShape->getSphereGeometry(geometry);
+	if (status)
+		SphereRadius = geometry.radius;
+	
+	//get the transform for this PhysX collision volume
+	PxMat44 m(PxShapeExt::getGlobalPose(*pShape, *actor));
+	glm::mat4 M(m.column0.x, m.column0.y, m.column0.z, m.column0.w,
+				m.column1.x, m.column1.y, m.column1.z, m.column1.w,
+				m.column2.x, m.column2.y, m.column2.z, m.column2.w,
+				m.column3.x, m.column3.y, m.column3.z, m.column3.w);
+
+	//get the position out of the transform
+	SpherePosition.x = m.getPosition().x;
+	SpherePosition.y = m.getPosition().y;
+	SpherePosition.z = m.getPosition().z;
+
+	if (actor->getName() != NULL && strcmp(actor->getName(), "Pickup1")) //seriously horrid hack so I can show pickups a different colour
+		SphereColour = glm::vec4(0, 1, 0, 1);
+
+	//create our sphere gizmo
+	Gizmos::addSphere(SpherePosition, SphereRadius, 10, 10, SphereColour, &M);
 }
 
 void Ragdoll::AddCapsule(PxShape* pShape, PxRigidActor* actor)
 {
-	//bool status = pShape->getCapsuleGeometry(capsuleGeometry);
-	//if (status)
-	//{
-	//	radius = capsuleGeometry.radius; //copy out capsule radius
-	//	halfHeight = capsuleGeometry.halfHeight; //copy out capsule half length
-	//}
-	//PxTransform transform = PxShapeExt::getGlobalPose(*pShape, *actor);
+	PxCapsuleGeometry capsuleGeometry;
+	
+	glm::vec3 cylinderPosition;
+	glm::vec4 colour = glm::vec4(1, 1, 0, 1);
+
+	float radius;
+	float halfHeight;
+
+	bool status = pShape->getCapsuleGeometry(capsuleGeometry);
+	if (status)
+	{
+		radius = capsuleGeometry.radius; //copy out capsule radius
+		halfHeight = capsuleGeometry.halfHeight; //copy out capsule half length
+	}
+
+	//get the transform for this PhysX collision volume
+	PxMat44 m(PxShapeExt::getGlobalPose(*pShape, *actor));
+	glm::mat4 M(m.column0.x, m.column0.y, m.column0.z, m.column0.w,
+				m.column1.x, m.column1.y, m.column1.z, m.column1.w,
+				m.column2.x, m.column2.y, m.column2.z, m.column2.w,
+				m.column3.x, m.column3.y, m.column3.z, m.column3.w);
+
+	//get the position out of the transform
+	cylinderPosition.x = m.getPosition().x;
+	cylinderPosition.y = m.getPosition().y;
+	cylinderPosition.z = m.getPosition().z;
+
+	if (actor->getName() != NULL && strcmp(actor->getName(), "Pickup1")) //seriously horrid hack so I can show pickups a different colour
+		colour = glm::vec4(0, 1, 0, 1);
+	
+	glm::vec4 axis(halfHeight, 0, 0, 0); //axis for the capsule
+	axis = M * axis; //rotate axis to correct orientation
+	//add our 2 end cap spheres...
+	Gizmos::addSphere(cylinderPosition + axis.xyz(), radius, 10, 10, colour);
+	Gizmos::addSphere(cylinderPosition - axis.xyz(), radius, 10, 10, colour);
+	//Fix the gizmo rotation
+	glm::mat4 m2 = glm::rotate(M, 11 / 7.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+	Gizmos::addCylinderFilled(cylinderPosition, radius, halfHeight, 10, colour, &m2);
 }
